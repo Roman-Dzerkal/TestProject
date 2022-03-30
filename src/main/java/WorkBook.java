d93644077ec436dc93c5f4e1bbf734f5b224531c
Roman Dzerkal <r.dzerkal@pinta.com.ua>
2022-03-30 13:07:41 +0300

    Init proejct

---
+++ src/main/java/WorkBook.java
@@ -0,0 +1,44 @@
+public class WorkBook {
+    private int age;
+    private String name;
+    private String surname;
+
+    WorkBook() {
+    }
+
+    WorkBook(int age, String name, String surname) {
+        this.age = age;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    private int sum(int value) {
+        return value * 2;
+    }
+}
